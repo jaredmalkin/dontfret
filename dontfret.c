bddb668df71c53597f2918cc750641bb830aac5e
Jared Malkin <jaredmalkin@jareds-mbp.dhcp.wustl.edu>
2018-04-14 13:00:56 -0500

    commented push buttons
    nice

--- dontfret.c
+++ dontfret.c
@@ -1,12 +1,9 @@
-// CC-by-www.Electrosmash.com open-source project.
-// distortion.c effect pedal, the input signal change is clipped depending on the PUSH1/PUSH2 buttons.
-
 #include <bcm2835.h>
 #include <stdio.h>
 
 // Define Input Pins
-#define PUSH1 			RPI_GPIO_P1_08  	//GPIO14
-#define PUSH2 			RPI_V2_GPIO_P1_38  	//GPIO20
+//#define PUSH1 			RPI_GPIO_P1_08  	//GPIO14
+//#define PUSH2 			RPI_V2_GPIO_P1_38  	//GPIO20
 #define TOGGLE_SWITCH 	RPI_V2_GPIO_P1_32 	//GPIO12
 #define FOOT_SWITCH 	RPI_GPIO_P1_10 		//GPIO15
 #define LED   			RPI_V2_GPIO_P1_36 	//GPIO16
@@ -25,8 +22,8 @@ uint32_t delay;
 
 uint8_t FOOT_SWITCH_val;
 uint8_t TOGGLE_SWITCH_val;
-uint8_t PUSH1_val;
-uint8_t PUSH2_val;
+//uint8_t PUSH1_val;
+//uint8_t PUSH2_val;
 
 int main(int argc, char **argv)
 {
@@ -59,14 +56,14 @@ int main(int argc, char **argv)
   uint8_t miso[10] = { 0 };
 
   //Define GPIO pins configuration
-  bcm2835_gpio_fsel(PUSH1, BCM2835_GPIO_FSEL_INPT); 			//PUSH1 button as input
-  bcm2835_gpio_fsel(PUSH2, BCM2835_GPIO_FSEL_INPT); 			//PUSH2 button as input
+  //bcm2835_gpio_fsel(PUSH1, BCM2835_GPIO_FSEL_INPT); 			//PUSH1 button as input
+  //bcm2835_gpio_fsel(PUSH2, BCM2835_GPIO_FSEL_INPT); 			//PUSH2 button as input
   bcm2835_gpio_fsel(TOGGLE_SWITCH, BCM2835_GPIO_FSEL_INPT);	//TOGGLE_SWITCH as input
   bcm2835_gpio_fsel(FOOT_SWITCH, BCM2835_GPIO_FSEL_INPT); 	//FOOT_SWITCH as input
   bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);				//LED as output
 
-  bcm2835_gpio_set_pud(PUSH1, BCM2835_GPIO_PUD_UP);           //PUSH1 pull-up enabled
-  bcm2835_gpio_set_pud(PUSH2, BCM2835_GPIO_PUD_UP);           //PUSH2 pull-up enabled
+  //bcm2835_gpio_set_pud(PUSH1, BCM2835_GPIO_PUD_UP);           //PUSH1 pull-up enabled
+  //bcm2835_gpio_set_pud(PUSH2, BCM2835_GPIO_PUD_UP);           //PUSH2 pull-up enabled
   bcm2835_gpio_set_pud(TOGGLE_SWITCH, BCM2835_GPIO_PUD_UP);   //TOGGLE_SWITCH pull-up enabled
   bcm2835_gpio_set_pud(FOOT_SWITCH, BCM2835_GPIO_PUD_UP);     //FOOT_SWITCH pull-up enabled
 
@@ -76,13 +73,14 @@ int main(int argc, char **argv)
     bcm2835_spi_transfernb(mosi, miso, 3);
     input_signal = miso[2] + ((miso[1] & 0x0F) << 8);
 
+    //Reading buttons and switches
     //Read the PUSH buttons every 50000 times (0.25s) to save resources.
     read_timer++;
     if (read_timer==50000)
     {
       read_timer=0;
-      uint8_t PUSH1_val = bcm2835_gpio_lev(PUSH1);
-      uint8_t PUSH2_val = bcm2835_gpio_lev(PUSH2);
+      //uint8_t PUSH1_val = bcm2835_gpio_lev(PUSH1);
+      //uint8_t PUSH2_val = bcm2835_gpio_lev(PUSH2);
       TOGGLE_SWITCH_val = bcm2835_gpio_lev(TOGGLE_SWITCH);
       uint8_t FOOT_SWITCH_val = bcm2835_gpio_lev(FOOT_SWITCH);
       //light the effect when the footswitch is activated.
