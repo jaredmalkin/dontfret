a8f869a8428daf4efd28cf9498a0149b590bdaef
jaredmalkin <35544560+jaredmalkin@users.noreply.github.com>
2018-05-01 15:45:38 +0000

    Update inputtest.c

--- testing/inputtest.c
+++ testing/inputtest.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
-#include <bcm2835.h>
+//clean.c effect pedal
 
-// Define Input Pins
-#define TOGGLE_SWITCH     RPI_V2_GPIO_P1_32   //GPIO12
-#define FOOT_SWITCH       RPI_GPIO_P1_10      //GPIO15
-#define LED               RPI_V2_GPIO_P1_36   //GPIO16
+#include <bcm2835.h>
+#include <stdio.h>
+
+//Define Input Pins
+#define PUSH1   RPI_GPIO_P1_08 //GPIO14
+#define PUSH2   RPI_V2_GPIO_P1_38 //GPIO20
+#define TOGGLE_SWITCH 	RPI_V2_GPIO_P1_32 	//GPIO12
+#define FOOT_SWITCH 	RPI_GPIO_P1_10 		//GPIO15
+#define LED   			RPI_V2_GPIO_P1_36 	//GPIO16
 
 uint32_t read_timer=0;
 uint32_t input_signal=0;
 
 uint8_t FOOT_SWITCH_val;
 uint8_t TOGGLE_SWITCH_val;
+uint8_t PUSH1_val;
+uint8_t PUSH2_val;
 
-
-//main program
 int main(int argc, char **argv)
 {
     // Start the BCM2835 Library to access GPIO.
@@ -40,16 +44,21 @@ int main(int argc, char **argv)
     bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64);    // 4MHz clock with _64
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+    printf("spi bus configuration defined");
 
     uint8_t mosi[10] = { 0x01, 0x00, 0x00 }; //12 bit ADC read channel 0.
     uint8_t miso[10] = { 0 };
 
     //Define GPIO pins configuration
+    bcm2835_gpio_fsel(PUSH1, BCM2835_GPIO_FSEL_INPT);           //PUSH1 button as input
+    bcm2835_gpio_fsel(PUSH2, BCM2835_GPIO_FSEL_INPT);           //PUSH2 button as input
     bcm2835_gpio_fsel(TOGGLE_SWITCH, BCM2835_GPIO_FSEL_INPT);   //TOGGLE_SWITCH as input
     bcm2835_gpio_fsel(FOOT_SWITCH, BCM2835_GPIO_FSEL_INPT);     //FOOT_SWITCH as input
     bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);             //LED as output
 
-    bcm2835_gpio_set_pud(TOGGLE_SWITCH, BCM2835_GPIO_PUD_UP);   //TOGGLE_SWITCH pull-up enabled
+    bcm2835_gpio_set_pud(PUSH1, BCM2835_GPIO_PUD_UP);           //PUSH1 pull-up enabled
+    bcm2835_gpio_set_pud(PUSH2, BCM2835_GPIO_PUD_UP);           //PUSH2 pull-up enabled
+    bcm2835_gpio_set_pud(TOGGLE_SWITCH, BCM2835_GPIO_PUD_UP);   //TOGGLE_SWITCH pull-up enabled 
     bcm2835_gpio_set_pud(FOOT_SWITCH, BCM2835_GPIO_PUD_UP);     //FOOT_SWITCH pull-up enabled
 
     while(1) //Main Loop
@@ -59,6 +68,10 @@ int main(int argc, char **argv)
     if (read_timer==50000)
     {
     read_timer=0;
+
+    //printf("would read buttons and switches here");
+    uint8_t PUSH1_val = bcm2835_gpio_lev(PUSH1);
+    uint8_t PUSH2_val = bcm2835_gpio_lev(PUSH2);
     TOGGLE_SWITCH_val = bcm2835_gpio_lev(TOGGLE_SWITCH);
     uint8_t FOOT_SWITCH_val = bcm2835_gpio_lev(FOOT_SWITCH);
     bcm2835_gpio_write(LED,!FOOT_SWITCH_val); //light the effect when the footswitch is activated.
@@ -66,14 +79,9 @@ int main(int argc, char **argv)
 
     //read 12 bits ADC
     bcm2835_spi_transfernb(mosi, miso, 3);
-
-    //print raw input before calculation
-    printf("miso[2]: " + miso[2] + "miso[1]: " + miso[1]);
     input_signal = miso[2] + ((miso[1] & 0x0F) << 8);
-
-    //Print input for testing. input_signal should be right around 2047,
-    //with min voltage returning 0 and max voltage returning 4095.
-    printf("input: " + input_signal);
+    printf("input: %d\n", input_signal);
+    //printf(miso[2], "+", ((miso[1]& 0x0F) << 8));
 
     //**** CLEAN EFFECT ***///
     //Nothing to do, the input_signal goes directly to the PWM output.
@@ -81,6 +89,7 @@ int main(int argc, char **argv)
     //generate output PWM signal 6 bits
     bcm2835_pwm_set_data(1,input_signal & 0x3F);
     bcm2835_pwm_set_data(0,input_signal >> 6);
+    //printf("output generated\n");
     }
 
     //close all and exit
